0327a7b1e3bac9de278c7aaa1f4ffc8f8c36a1ea
Zackary Wika <zwika@Zackarys-Air.attlocal.net>
2020-10-07 11:40:25 -0500

    Update links

--- assets/partials/modal-shortcode-inline-link.php
+++ assets/partials/modal-shortcode-inline-link.php
@@ -5,7 +5,7 @@
  *
  * @author   LUNCH LADY <support@lunchlady.co>
  * @license  https://www.gnu.org/licenses/gpl-2.0.en.html GPL-2.0
- * @link     https://lunchlady.co/
+ * @link     https://github.com/heavymetaltrays/modal-plugin/
  * @version  1.0.0
  *
  * @package  Modal
